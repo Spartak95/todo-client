866e3faa32ab391317d144c6547c0b5be051a4e5
spartak.gareginyan <gareginyans@mail.ru>
2023-11-01 10:00:19 +0400

    finish writing the logic for the ToDoRestClient class.

--- src/main/java/com/apress/todo/ToDoRestClient.java
+++ src/main/java/com/apress/todo/ToDoRestClient.java
@@ -1,9 +1,11 @@
 package com.apress.todo;
 
 import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Map;
 
 import com.apress.todo.domain.ToDo;
 import com.apress.todo.error.ToDoErrorHandler;
@@ -28,13 +30,43 @@ public class ToDoRestClient {
     public Iterable<ToDo> findAll() throws URISyntaxException {
         URI url = new URI(properties.getUrl() + properties.getBasePath());
         RequestEntity<Iterable<ToDo>> requestEntity = new RequestEntity<>(GET, url);
-        ResponseEntity<Iterable<ToDo>> response =
-            restTemplate.exchange(requestEntity, new ParameterizedTypeReference<>() {});
+        ResponseEntity<Iterable<ToDo>> response = restTemplate.exchange(requestEntity,
+                                                                        new ParameterizedTypeReference<>() {
+                                                                        });
 
         if (response.getStatusCode() == HttpStatus.OK) {
             return response.getBody();
         }
 
         return null;
+    }
+
+    public ToDo findById(String id) {
+        String url = properties.getUrl() + properties.getBasePath() + "/{id}";
+
+        return restTemplate.getForObject(url, ToDo.class, Map.of("id", id));
+    }
+
+    public ToDo upsert(ToDo toDo) throws URISyntaxException {
+        URI url = new URI(properties.getUrl() + properties.getBasePath());
+        RequestEntity<?> requestEntity = new RequestEntity<>(toDo, POST, url);
+        ResponseEntity<?> response = restTemplate.exchange(requestEntity, new ParameterizedTypeReference<ToDo>() {
+        });
+
+        if (response.getStatusCode() == HttpStatus.CREATED) {
+            return restTemplate.getForObject(response.getHeaders().getLocation(), ToDo.class);
+        }
+
+        return null;
+    }
+
+    public ToDo setCompleted(String id) {
+        restTemplate.patchForObject(properties.getUrl() + properties.getBasePath() + "/{id}?_method=patch", null,
+                                    ResponseEntity.class, Map.of("id", id));
+        return findById(id);
+    }
+
+    public void delete(String id) {
+        restTemplate.delete(properties.getUrl() + properties.getBasePath() + "/{id}", Map.of("id", id));
     }
 }
--- src/main/java/com/apress/todo/TodoClientApplication.java
+++ src/main/java/com/apress/todo/TodoClientApplication.java
@@ -1,13 +1,37 @@
 package com.apress.todo;
 
+import com.apress.todo.domain.ToDo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+@Slf4j
 @SpringBootApplication
 public class TodoClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodoClientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TodoClientApplication.class, args);
+    }
 
+    @Bean
+    public CommandLineRunner process(ToDoRestClient client) {
+        return args -> {
+            Iterable<ToDo> toDos = client.findAll();
+            assert toDos != null;
+            toDos.forEach(toDo -> log.info(toDo.toString()));
+            ToDo newToDo = client.upsert(new ToDo("Drink plenty of Water daily!"));
+            assert newToDo != null;
+            log.info(newToDo.toString());
+            ToDo toDo = client.findById(newToDo.getId());
+            assert toDos != null;
+            log.info(toDo.toString());
+            ToDo completed = client.setCompleted(newToDo.getId());
+            assert completed.isCompleted();
+            log.info(completed.toString());
+            client.delete(newToDo.getId());
+            assert client.findById(newToDo.getId()) == null;
+        };
+    }
 }
